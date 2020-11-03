ae368714aaf2c47fac91ef027059f0333993a203
naasyakoot <naasyakoot@outlook.com>
2020-11-03 18:12:36 +0600

    test 4 completed

--- src/logoandreg.js
+++ src/logoandreg.js
@@ -11,40 +11,7 @@ var data = function data() {
 
 
 		var template = `
-		 <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-sm-3">
-                        <div class="logo">
-                            <a href="https://themesbazar.com/"><img src="https://themesbazar.com/wp-content/themes/ThemesbazarTheme/assets/img/ThemesBazar.jpg" alt="Themes Bazar" title="Themes Bazar"></a>
-                        </div>
-                    </div>
-                    
-                    <div class="col-md-4 col-sm-3">
-                        <div class="support">
-                            <i class="fa fa-cubes"></i>
-                           <h4><a href="https://themesbazar.com/affiliate/"> এফিলিয়েট করুন</a></h4>
-                           <h5><a href="https://themesbazar.com/affiliate/">এফিলিয়েট করে আয় করুন</a></h5>
-                        </div>
-                    </div>    
-                       
-                    <div class="col-md-3 col-sm-3">
-                        <div class="support">
-                           <i class="fa fa-lock"></i>
-                           <h4><a href="https://popular-it.com/register.php" target="_blank">লগ ইন / সাইন আপ </a></h4>
-                           <h5><a href="https://popular-it.com/register.php" target="_blank">আমাদের সাথে থাকুন</a></h5>
-                        </div>
-                    </div> 
-                    
-                                   
-                    <div class="col-md-2 col-sm-3">
-                        <div class="english-verson">
-                            <a href="https://themesbazar.com/en" target="_blank"> English Version</a>
-                        </div>
-                                            
-                    </div>
-                    
-                </div>
-            </div>
+		
 
             `;
 
@@ -55,7 +22,7 @@ var app = Vue.createApp({ data, template });
 
 	*/
 
-	var sets = { data, template };
+	var sets = { data };
 
 	Vue.createApp(sets).mount('.logo-reg-section');
 
