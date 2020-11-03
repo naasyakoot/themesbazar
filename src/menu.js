6e0510d709e9a474e39a97a238d3096ce35c388a
naasyakoot <naasyakoot@outlook.com>
2020-11-03 11:09:28 +0600

    loop v-for done

--- src/menu.js
+++ src/menu.js
@@ -6,7 +6,7 @@ var data = function data() {
 
         name: "SALAMOON ALAIKOOM",
         name2: "SALAMOON SALAMOON",
-        some: "thing"
+        some: "thing",
 
       }
 
@@ -14,25 +14,7 @@ var data = function data() {
     };
 
 
-    var template = `
-
-    <a class="py-2 d-none d-md-inline-block" href="#">নিউজপেপার</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">টিভি থিম</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">প্রতিষ্ঠান</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">ই-পেপার</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">ই-কমার্স</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">বিজনেস</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">ব্লগ</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">প্লাগিন</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">পিএইচপি সাইট</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">পার্সোনাল</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">অন্যান্য থিম</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">ভিডিও টিউটোরিয়াল</a>
-
-
-  
-
-            `;
+    var template = `{{some}}`;
 
 /*
 var app = Vue.createApp({ data, template });
@@ -41,7 +23,7 @@ var app = Vue.createApp({ data, template });
 
   */
 
-  var sets = { data, template };
+  var sets = { data };
 
   Vue.createApp(sets).mount('.mmin');
 
